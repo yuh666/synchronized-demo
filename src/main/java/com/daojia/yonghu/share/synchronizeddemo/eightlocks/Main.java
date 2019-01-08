cfc6d9d9cc01a4e248a8eb5f55bb45df72a3b56f
yuhao <yuhao@daojia-inc.com>
2019-01-08 17:02:39 +0800

    add

--- src/main/java/com/daojia/yonghu/share/synchronizeddemo/eightlocks/Main.java
+++ src/main/java/com/daojia/yonghu/share/synchronizeddemo/eightlocks/Main.java
@@ -9,26 +9,37 @@ public class Main {
     /*
      * 题目：判断打印的 "one" or "two" ？
      *
-     * 1. 两个普通同步方法，两个线程，标准打印， 打印? //one  two
-     * 2. 新增 Thread.sleep() 给 getOne() ,打印? //one  two
-     * 3. 新增普通方法 getThree() , 打印? //three  one   two
-     * 4. 两个普通同步方法，两个 Number 对象，打印?  //two  one
-     * 5. 修改 getOne() 为静态同步方法，打印?  //two   one
-     * 6. 修改两个方法均为静态同步方法，一个 Number 对象?  //one   two
-     * 7. 一个静态同步方法，一个非静态同步方法，两个 Number 对象?  //two  one
-     * 8. 两个静态同步方法，两个 Number 对象?   //one  two
+     * 1. 两个普通同步方法  两个线程    一个number对象  打印?
+     * 2. 新增 Thread.sleep() 给 getOne()  两个线程    一个number对象  打印?
+     * 3. 新增普通方法 getThree()     三个线程    一个number对象  打印?
+     * 4. 两个普通同步方法  两个Number对象  两个线程 一个number对象 打印?
+     * 5. 修改 getOne()为静态同步方法    一个number对象  打印?
+     * 6. 修改两个方法均为静态同步方法，一个 Number 对象?
+     * 7. 一个静态同步方法  一个非静态同步方法   两个 Number 对象  打印?
+     * 8. 两个静态同步方法  两个 Number 对象  打印?
      *
      */
+
+    static class Number {
+        public void getOne() {
+            System.out.println("one");
+        }
+
+        public synchronized void getTwo() {
+            System.out.println("two");
+        }
+
+        public void getThree() {
+            System.out.println("three");
+        }
+    }
 
     public static void main(String[] args) {
         Number number = new Number();
         Number number2 = new Number();
 
         new Thread(() -> number.getOne()).start();
-        new Thread(() -> {
-            number.getTwo();
-            number2.getTwo();
-        }).start();
-        new Thread(() -> number.getThree()).start();
+        new Thread(() -> number2.getTwo()).start();
+//        new Thread(() -> number.getThree()).start();
     }
 }
